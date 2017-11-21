0a51d0ac54ed79d9b994e44f72aa9e3ba6d46b90
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-21 19:10:35 +0000

    automatically backed up by learn

--- db/migrate/20171121181032_create_posts.rb
+++ db/migrate/20171121181032_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.text :title
-      t.text :blog
+      t.string :title
+      t.string :blog
     end
   end
 end
