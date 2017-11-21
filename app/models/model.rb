d6b758a982a080aed8d18b43194abd1521303fe1
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-21 18:44:30 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -2,14 +2,10 @@
 class Post < ActiveRecord::Base
 
   attr_accessor :title, :blog
-  
+
   def initialize(title, blog)
     @title = title
     @blog = blog
-  end
-
-  def self.create(title, blog)
-    posts = Post.new(title, blog)
   end
 
 end
