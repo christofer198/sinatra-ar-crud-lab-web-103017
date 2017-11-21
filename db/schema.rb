fc0157d9658d8568d36f124f0f999bbf1b9c3e2c
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-21 20:35:53 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121181032) do
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "blog"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
