7925b1465c15d606c9caa15d0f4b9d2a4d104f18
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-21 19:11:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,7 @@ class ApplicationController < Sinatra::Base
   post '/posts' do
     new_post = Post.new
     new_post.title = params[:title]
-    new_post.blog = params[:blog]
-    new_post.save
+    new_post.title
   end
 
 end
