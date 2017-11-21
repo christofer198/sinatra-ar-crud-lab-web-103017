1e9bdc23cc48f828b976fe8baa6a920c73a9b8fd
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-21 18:39:39 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -2,5 +2,12 @@
 class Post < ActiveRecord::Base
 
   attr_accessor :title, :blog
+  def initialize(title, blog)
+    @title = title
+    @blog = blog
+  end
+  def self.create(title, blog)
+
+  end
 
 end
